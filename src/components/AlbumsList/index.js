7811e65bbf74238e3ebe2ae2f1aab35a1a193a78
Jefferson Moraes <jeff.dev.moraes@gmail.com>
2020-09-26 12:24:43 +0200

    Component: AlbumsList = add itunes IPA request

--- src/components/AlbumsList/index.js
+++ src/components/AlbumsList/index.js
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function AlbumsList() {
+  const [albums, setAlbums] = useState([]);
+
+  const loadAlbums = async () => {
+    const albumsFromiTunesAPI = await axios.get(
+      "https://itunes.apple.com/us/rss/topalbums/limit=100/json"
+    );
+    console.log(albumsFromiTunesAPI.data.feed.entry, "albumsFromiTunesAPI");
+    setAlbums(albumsFromiTunesAPI.data.feed.entry);
+  };
+
+  useEffect(() => {
+    loadAlbums();
+  }, []);
+
   return (
     <div>
       <h1>AlbumsList</h1>
